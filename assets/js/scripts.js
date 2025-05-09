b2251e0a60062d68a99b333897aec46b082d11a8
libertus-libertus <libert.jobs@gmail.com>
2025-05-09 13:56:28 +0700

    Updated responsive

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,10 +2,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     const header = document.getElementById('main-header');
     const body = document.body;
-    
+
     // Threshold when the navbar should become fixed
     const scrollThreshold = 100;
-    
+
     window.addEventListener('scroll', function() {
         if (window.scrollY > scrollThreshold) {
             header.classList.add('fixed-navbar');
